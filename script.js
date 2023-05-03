8ca7ae3b816d87ca7ee09e41a9576aa471e50c7b
juancamilobanguero <banguerojuancamilo58@gmail.com>
2023-05-03 15:31:20 -0500

    pokemon12

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const pokemonContainer = document.querySelector(`.pokemon-container`);
+const spinner = document.querySelector("#spinner");
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+let offset = 1;
+let limit = 8;
+
+previous.addEventListener(`click`,() => {
+    if(offset  != 1){
+        offset -= 9;
+        removeChildNodes(pokemonContainer);
+        fetchpokemons(offset,limit);
+    }
+   
+})
+
+next.addEventListener(`click`,() => {
+    offset += 9;
+    removeChildNodes(pokemonContainer);
+    fetchpokemons(offset,limit);
+})
 
 function fetchpokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(res => res.json())
     .then(data => {
+    //    console.log( data );
        createpokemon(data)
+       spinner.style.display = "none";
     });
 }
 
-function fetchpokemons(number){
-    for(let i = 1; i <= number; i++){
+function fetchpokemons(offset, limit){
+    spinner.style.display = "block";
+    for(let i = offset; i <= offset + limit; i++){
         fetchpokemon(i);
     }
 }
@@ -22,9 +46,13 @@ function createpokemon(pokemon) {
     spriteContainer.classList.add("img-container");
 
     const sprite = document.createElement(`img`);
-    sprite.src = pokemon.sprites.front_default
+
+    sprite.setAttribute( 'src', pokemon.sprites.front_default );
+    sprite.setAttribute( 'alt', pokemon.name );
 
     spriteContainer.appendChild(sprite);
+
+    console.log( spriteContainer );
 
     const number = document.createElement("p");
     number.textContent = `#${pokemon.id.toString().padStart(3,0)}`
@@ -35,7 +63,7 @@ function createpokemon(pokemon) {
     name.textContent = pokemon.name;
 
 
-    // card.apendChild(spriteContainer);
+    card.appendChild(spriteContainer);
     card.appendChild(number);
     card.appendChild(name);
    
@@ -43,4 +71,10 @@ function createpokemon(pokemon) {
     pokemonContainer.appendChild(card);
 }
 
-fetchpokemons(9);
+function removeChildNodes(parent){
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+fetchpokemons(offset,limit);
